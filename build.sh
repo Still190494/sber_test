e7365617d50e2c34c01984d5babb58030ddef686
Still190494 <Still190494@ya.ru>
2024-04-25 22:15:03 +0300

    try14

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 set -o errexit  # exit on error
-
+pip install --upgrade pip
 pip install -r requirements.txt
-
+cd nn_map
 python manage.py collectstatic --no-input
 python manage.py migrate
